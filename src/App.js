12eb6159bbb75d59c859f7db02a6c19bbeb69975
Audun Haugland <audun@netlab.no>
2022-01-05 10:58:18 +0100

    Minor changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//import moment from "moment";
 
-function App() {
+import AppSearchAPIConnector from "@elastic/search-ui-app-search-connector";
+
+
+import {
+  ErrorBoundary,
+  Facet,
+  SearchProvider,
+  SearchBox,
+  Results,
+  PagingInfo,
+  ResultsPerPage,
+  Paging,
+  Sorting,
+  WithSearch
+} from "@elastic/react-search-ui";
+import {
+  //BooleanFacet,
+  Layout,
+  //SingleSelectFacet,
+  //SingleLinksFacet
+} from "@elastic/react-search-ui-views";
+import "@elastic/react-search-ui-views/lib/styles/styles.css";
+
+const SORT_OPTIONS = [
+  {
+    name: "Order by",
+    value: "",
+    direction: ""
+  },
+  {
+    name: "Title",
+    value: "title",
+    direction: "asc"
+  }
+];
+
+const connector = new AppSearchAPIConnector({
+  searchKey: "",
+  engineName: "search-ui",
+  endpointBase: "https://ooe.5.erkunde.no/site/ooe/",
+  cacheResponses: true
+});
+
+const config = {
+  alwaysSearchOnInitialLoad: true,
+  searchQuery: {
+    language: window.language,
+    contentType: window.contentType,
+    contextFilter: window.contextFilter,
+    returnFacets: window.returnFacets
+  },
+  autocompleteQuery: {
+    language: window.language
+  },
+  apiConnector: connector
+};
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SearchProvider config={config}>
+      <WithSearch mapContextToProps={({ wasSearched }) => ({ wasSearched })}>
+        {({ wasSearched }) => {
+          return (
+            <div className="App">
+              <ErrorBoundary>
+                <Layout
+                  header={
+                    <SearchBox
+                      autocompleteMinimumCharacters={3}
+                      //searchAsYouType={true}
+                      autocompleteResults={{
+                        linkTarget: "_blank",
+                        sectionTitle: "Results",
+                        titleField: "title",
+                        urlField: "nps_link",
+                        shouldTrackClickThrough: true,
+                        clickThroughTags: ["test"]
+                      }}
+                      autocompleteSuggestions={true}
+                      debounceLength={0}
+                    />
+                  }
+                  sideContent={
+                    <div>
+                      <div id="facets" className="facets">
+                        {false && wasSearched && (
+                          <Sorting sortOptions={SORT_OPTIONS} />
+                        )}
+
+                        <Facet
+                          field="category"
+                          label=""
+                          filterType="any"
+                          //view={SingleLinksFacet}
+                          //isFilterable={true}
+                        />
+                      </div>
+                    </div>
+                  }
+                  bodyContent={
+                    <Results
+                      titleField="title"
+                      urlField="itemurl"
+                      thumbnailField="image_url"
+                      shouldTrackClickThrough={true}
+                    />
+                  }
+                  bodyHeader={
+                    <React.Fragment>
+                      {wasSearched && <PagingInfo />}
+                      {wasSearched && <ResultsPerPage />}
+                    </React.Fragment>
+                  }
+                  bodyFooter={<Paging />}
+                />
+              </ErrorBoundary>
+            </div>
+          );
+        }}
+      </WithSearch>
+    </SearchProvider>
   );
 }
-
-export default App;
